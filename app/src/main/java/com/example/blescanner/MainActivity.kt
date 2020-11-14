2d3b394f289e51267b8a5ef96fb3269189e73b05
amy6 <mahima.6.ms@gmail.com>
2020-11-14 09:59:15 +0530

    Add Fragment and ViewModel classes Add RecyclerView to list scanned devices in MainFragment Setup data binding

--- app/src/main/java/com/example/blescanner/MainActivity.kt
+++ app/src/main/java/com/example/blescanner/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.blescanner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.blescanner.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
     }
 }
---
+++ app/src/main/java/com/example/blescanner/ui/main/MainFragment.kt
@@ -0,0 +1,41 @@
+package com.example.blescanner.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.blescanner.R
+import com.example.blescanner.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            LayoutInflater.from(context),
+            R.layout.fragment_main,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
